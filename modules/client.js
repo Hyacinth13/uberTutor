31a945bf15124c5464df947c7c391a4739b7862c
Hyacinth13 <jpeckwriter@gmail.com>
2016-04-29 09:39:52 -0600

    updated modules/client.js with redux auth info

--- modules/client.js
+++ modules/client.js
@@ -2,9 +2,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import routes from '../modules/routes'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
+
+const createStoreWithMiddleware = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)(createStore)
+
+const store = createStoreWithMiddleware(rootReducer)
 
 render(
-  <Router history={browserHistory} routes={routes}/>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
   document.getElementById('app')
 )
 
