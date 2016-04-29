9a8e3e3a6597c35f0634e9d5b0d2617f687ae7bd
Hyacinth13 <jpeckwriter@gmail.com>
2016-04-29 08:51:05 -0600

    added passport and mongoose to server.js

--- modules/server.js
+++ modules/server.js
@@ -4,6 +4,13 @@ import { RouterContext } from 'react-router'
 import Document from '../modules/components/Document'
 import routes from '../modules/routes'
 
+import mongoose from 'mongoose'
+import passport from 'passport'
+import local from 'passport-local'
+import session from 'express-session'
+import User from './models/user'
+let LocalStrategy = local.Strategy
+
 function getApp(req, res, requestCallback) {
   // here is your chance to do things like get an auth token and generate
   // your route config w/ request aware `onEnter` hooks, etc.
@@ -20,5 +27,16 @@ function getApp(req, res, requestCallback) {
   })
 }
 
-createServer(getApp).start()
+const server = createServer(getApp)
+
+server.use( session({ secret: 'secret', resave: false, saveUnitialized: false }))
+server.use(passport.initialize())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+mongoose.connect('mongodb://localhost/auth-3')
+
+server.start()
 
