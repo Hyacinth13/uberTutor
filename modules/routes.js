f2a5a2dd83e391be92c2cbcfec118bb73a7efee2
Hyacinth13 <jpeckwriter@gmail.com>
2016-04-29 09:34:05 -0600

    updated routes with authorization

--- modules/routes.js
+++ modules/routes.js
@@ -1,23 +1,36 @@
 import '../modules/styles.css'
 import React from 'react'
-import { Route, IndexRoute, Redirect } from 'react-router'
+import { Route } from 'react-router'
 import { ServerRoute } from 'react-project'
-import hello from './api/hello'
 import App from './components/App'
-import Home from './components/Home'
 import NoMatch from './components/NoMatch'
-import Dragon from './components/Dragon'
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import About from './components/About'
+import { signUp, signIn } from './api/auth'
+import { createTodo, getTodos } from './api/todos'
+import { UserAuthWrapper } from 'redux-auth-wrapper'
+import { push } from 'react-router-redux'
+
+const UserIsAuthenticated = UserAuthWrapper({
+  authSelector: state => state.auth,
+  predicate: auth => auth.isAuthenticated,
+  redirectAction: push,
+  wrapperDisplayName: 'UserIsAuthenticated'
+})
 
 export default (
   <Route>
     <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="dragon" component={Dragon}/>
+      <Route path="login" component={Login} />
+      <Route path="about" component={About} />
+      <Route path="dashboard" component={UserIsAuthenticated(Dashboard)} />
     </Route>
     <ServerRoute path="/api">
-      <ServerRoute path=":hello" get={hello}/>
+      <ServerRoute path="signup" post={signUp} />
+      <ServerRoute path="signin" post={signIn} />
+      <ServerRoute path="todos" post={createTodo} get={getTodos} />
     </ServerRoute>
-    <Redirect from="/not-dragon" to="/dragon"/>
     <Route path="*" status={404} component={NoMatch}/>
   </Route>
 )
