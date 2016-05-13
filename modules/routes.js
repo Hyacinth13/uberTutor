9237b56175adacc59ad8ddf55f034c8fd79beda5
Hyacinth13 <jpeckwriter@gmail.com>
2016-05-12 18:41:02 -0600

    added Dashboard to routes file, uncommented user auth

--- modules/routes.js
+++ modules/routes.js
@@ -4,27 +4,28 @@ import { Route, IndexRoute } from 'react-router'
 import { ServerRoute } from 'react-project'
 import App from './components/App'
 import Login from './components/Login'
-import Signup from './components/Signup'
-
-//import { signUp, signIn } from './api/auth'
+import Dashboard from './components/Dashboard'
+import { signUp, signIn } from './api/auth'
 import { UserAuthWrapper } from 'redux-auth-wrapper'
 import { push } from 'react-router-redux'
 
-//const UserIsAuthenticated = UserAuthWrapper({
-//  authSelector: state => state.auth,
-//  predicate: auth => auth.isAuthenticated,
-//  redirectAction: push,
-//  wrapperDisplayName: 'UserIsAuthenticated'
-//})
+const UserIsAuthenticated = UserAuthWrapper({
+ authSelector: state => state.auth,
+ predicate: auth => auth.isAuthenticated,
+ redirectAction: push,
+ wrapperDisplayName: 'UserIsAuthenticated'
+})
 
 export default (
   <Route>
     <Route path="/" component={App}>
       <IndexRoute component={Signup} />
       <Route path="login" component={Login} />
-      <Route path="signup" component={Signup} />
+      <Route path="dashboard" component={UserIsAuthenticated(Dashboard)} />
     </Route>
     <ServerRoute path="/api">
+      <ServerRoute path="signup" post={signUp} />
+      <ServerRoute path="signin" post={signIn} />
     </ServerRoute>
   </Route>
 )
