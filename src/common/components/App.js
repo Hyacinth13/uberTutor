bc4303486cf9a092a38cb2a4d2dd95a6a78fa38f
Hyacinth13 <jpeckwriter@gmail.com>
2016-05-25 18:55:43 -0600

    added signout functionallity

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
-import { logout, loggedIn } from '../actions/authActions'
+import { signOut, loggedIn } from '../actions/authActions'
 import { Navbar, NavItem } from 'react-materialize'
 import Signup from './Signup'
 import Login from './Login'
@@ -9,6 +9,11 @@ import Login from './Login'
 class App extends Component {
   constructor(props) {
     super(props)
+  }
+
+  handleSignOut() {
+    const { dispatch } = this.props
+    dispatch(authActions.signOut())
   }
 
   // componentWillMount() {
@@ -25,6 +30,7 @@ class App extends Component {
             <div className={"black nav-wrapper"}>
               <a href="#" className="brand-logo white-text accent-3">Uber Tutor</a>
               <ul id="nav-mobile" className={"right hide-on-med-and-down"}>
+                <NavItem><Link className={"white-text accent-3"} to={this.handleSignOut}>Sign Out</Link></NavItem>
                 <NavItem><Link className={"white-text accent-3"} to="/login">Login</Link></NavItem>
                 <NavItem><Link className={"white-text accent-3"} to="/tutor">Find a Tutor</Link></NavItem>
                 <NavItem><Link className={"white-text accent-3"} to="/help">Help</Link></NavItem>
