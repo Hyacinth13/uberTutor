6f4aed87be839306dcada08b1d3cf0e3ea6daac0
Hyacinth13 <jpeckwriter@gmail.com>
2016-05-26 17:58:44 -0600

    added logic to make the navbar change signin/signout link based on user status

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
-import { signOut, loggedIn } from '../actions/authActions'
+import { checkAuth, signOut, loggedIn } from '../actions/authActions'
 import { Navbar, NavItem } from 'react-materialize'
 import Signup from './Signup'
 import Login from './Login'
+import cookie from 'react-cookie'
 
 class App extends Component {
   constructor(props) {
@@ -21,7 +22,12 @@ class App extends Component {
   //     this.props.dispatch(loggedIn(sessionStorage.userId, sessionStorage.token))
   // }
 
+  // <NavItem><Link className={"white-text accent-3"} to={this.handleSignOut}>Sign Out</Link></NavItem>
+  // <NavItem><Link className={"white-text accent-3"} to="/login">Login</Link></NavItem>
+                
+
   render() {
+    let link = cookie.load('username') ? (<Link className={"white-text accent-3"} to={this.handleSignOut}>Sign Out</Link>) : (    <Link className={"white-text accent-3"} to="/login">Login</Link>)
     return (
 
       <div>
@@ -30,8 +36,7 @@ class App extends Component {
             <div className={"black nav-wrapper"}>
               <a href="#" className="brand-logo white-text accent-3">Uber Tutor</a>
               <ul id="nav-mobile" className={"right hide-on-med-and-down"}>
-                <NavItem><Link className={"white-text accent-3"} to={this.handleSignOut}>Sign Out</Link></NavItem>
-                <NavItem><Link className={"white-text accent-3"} to="/login">Login</Link></NavItem>
+                {link}
                 <NavItem><Link className={"white-text accent-3"} to="/tutor">Find a Tutor</Link></NavItem>
                 <NavItem><Link className={"white-text accent-3"} to="/help">Help</Link></NavItem>
               </ul>
